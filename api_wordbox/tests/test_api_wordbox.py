0486a01cf16aac4eee5ce2c54897da68710b951c
Adrián Vélez <a.anselmo.velez@gmail.com>
2021-12-05 13:35:55 -0500

    test commit

---
+++ api_wordbox/tests/test_api_wordbox.py
@@ -0,0 +1,29 @@
+import json
+import requests
+from api_wordbox import __version__
+
+
+def test_version():
+    assert __version__ == '0.1.0'
+
+def test_signup():
+    data={
+        'password':'pass_prueba',
+        'name':'Prueba',
+        'last_name':'prueba',
+        'username':'prueba',
+        'address':'carrera 8 # 2 - 12',
+        'phone1':'123456',
+        'phone2':'654321'
+    }
+    response = requests.post('localhost:5000/sign_up', data=data)
+    assert json.loads(response.content)['Success'] == 'User has been signed up'
+
+def test_login():
+
+    data={
+        'username':'prueba',
+        'password':'pass_prueba'
+    }
+    response = requests.post('localhost:5000/login', data=data)
+    assert response.content == 'User correctly logged in'
